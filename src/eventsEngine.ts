c07de8679ea2696077678a288b924baed255d9b3
Neil Dewhurst <neil.dewhurst@neo4j.com>
2022-05-24 18:06:53 +0200

    add knowledge

--- src/eventsEngine.ts
+++ src/eventsEngine.ts
@@ -1,12 +1,17 @@
 import { Neo4jAPI } from "./neo4j_api";
 import { Queries } from "./queries";
+import { Knowledge } from "./knowledge";
+
+import { h, $ } from "./dom";
 
 export class EventsEngine {
     private events = [
         {
             conditions: [Queries.isLightOn],
-            effects: [Queries.createSafe, Queries.createCupboard],
-            effectText: "You turn the light on, the room is nasty"
+            effects: [Queries.createSafe, Queries.createCupboard, Queries.updateLightSwitchDesc],
+            effectText: "You turn the light on, the room is nasty",
+            knowledge: [Knowledge.lightSwitch],
+            cypher: ["SET"]
         }
     ]
 
@@ -22,6 +27,8 @@ export class EventsEngine {
         for(const event of this.events){
             if(await this.runConditions(event.conditions)){
                 await this.runEffects(event.effects);
+                await this.addKnowledge(event.knowledge);
+                await this.addCypherKnowledge(event.cypher);
                 console.log(event.effectText);
 
             } else {
@@ -45,6 +52,23 @@ export class EventsEngine {
     public async runEffects(effects:Array<string>): Promise<void>{
         await Promise.all(effects.map(async (query)=>{
             await this.api.runCypher(query)
+        }))
+    }
+
+    public async addKnowledge(knowledge:Array<string>): Promise<void>{
+        await Promise.all(knowledge.map(async (knowledge)=>{
+            console.log(knowledge)
+        }))
+    }
+
+    public async addCypherKnowledge(cypher:Array<string>): Promise<void>{
+        await Promise.all(cypher.map(async (cypher)=>{
+            console.log(cypher)
+            const learned = $(".learned");
+            learned.append(
+                h("p", null, cypher)
+              );
+
         }))
     }
 }
--- src/queries.ts
+++ src/queries.ts
@@ -9,5 +9,8 @@ export const Queries =
     //Create objects
     createLightSwitch : `CREATE (lightSwitch:LightSwitch {on: false, description: "Looks like a common light switch. Also works like one."})`,
     createSafe: `CREATE (:Safe {locked: true, description:"A wooden chest with metal structures."})`,
-    createCupboard: `CREATE (cupboard:Cupboard {description:"A wooden cupboard. It matches with the bed."})`
+    createCupboard: `CREATE (cupboard:Cupboard {description:"A wooden cupboard. It matches with the bed."})`,
+
+    //Update objects
+    updateLightSwitchDesc: `MATCH (l:LightSwitch) SET l.description = 'A light switch to turn the room light on or off. This much you have figured out...'`
 }
